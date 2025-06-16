aebeba5b922a7aa2fc6a1778081b86cdc490c253
arpan-jk <arpan.koley@jktech.com>
2025-06-16 08:07:04 +0000

    func modified

--- graph.py
+++ graph.py
@@ -1,6 +1,6 @@
 from processes.process import Process
 
-from utils import END
+from utils import END,get_random_number,MAX_ATTEMPT
 
 class Graph:
     def __init__(self, input_array: list[int]):
@@ -17,6 +17,8 @@ class Graph:
 
     def run(self, start_process: str):
 
+        retry_count = {}
+
         def dfs(process_name: str) -> int | None:
             if process_name not in self.processes:
                 print(f"[Error] Process '{process_name}' not found.")
@@ -28,11 +30,32 @@ class Graph:
             result = process.run(self.array)
 
             if result is not None:
-                self.array.append(result)
+                random_int = get_random_number()
+                print(f"[GRAPH] random number added = {random_int}")
+                self.array.append(random_int)
+                print(f"[GRAPH] new list = {self.array}")
+
+          
+
+            retry_count[process_name] = retry_count.get(process_name, 1)
+            
+           
 
             for condition, next_process in self.edges.get(process_name, []):
-                if condition(result):
-                    return dfs(next_process)
+                if retry_count[process_name] < MAX_ATTEMPT and condition(result):
+                    if next_process == process_name:
+                        retry_count[process_name] += 1
+                    else :
+                        return dfs(next_process)
+                    # return dfs(next_process)
+                elif retry_count[process_name] >= MAX_ATTEMPT:
+                    print(f"[GRAPH] Max attempt reached for {process_name}.Moving to the next process")
+                    continue
+                else:
+                    print("[GRAPH] Moving to next process")
+                    continue
+                
+                return dfs(next_process)
 
             return result
 
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 
+import random
 
 # Conditions
 is_even = lambda x: x is not None and x % 2 == 0
@@ -7,3 +8,9 @@ move_direct = lambda x: True
 
 # Process names
 ADD, MULT, DIV, START, END = "ADD", "MULT", "DIV", "START", "END"
+
+MAX_ATTEMPT = 2
+
+def get_random_number(start: int = 1, end: int = 1000) -> int:
+    return random.randint(start, end)
+
